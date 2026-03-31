55fbb56b245b61791ab1097e79834d4da81ee9d0
Job Savelsberg <jobsavelsberg@gmail.com>
2026-03-31 13:51:51 +0200

    Make age calculation actually update by running it in browser

---
+++ src/components/Age.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    const birthday = new Date("1997-03-09");
+
+    function fullYearsBetween(date1: Date, date2: Date): number {
+        let years = date2.getFullYear() - date1.getFullYear();
+        const monthDiff = date2.getMonth() - date1.getMonth();
+        const dayDiff = date2.getDate() - date1.getDate();
+        if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+            years--;
+        }
+        return years;
+    }
+
+    const age = fullYearsBetween(birthday, new Date());
+</script>
+
+{age}
